cc0d1abad78d4a20601025fc98e04052fd325f63
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-15 19:54:29 +0530

    Create multiple.cpp

---
+++ multiple.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int s=0,n;
+    cin>>n;
+    for(int i=1;i<=5;i++)
+    {
+        s=i*n;
+        cout<<s<<" ";
+    }
+    return 0;
+}
