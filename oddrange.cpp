1f800d6f3952bcd842d8481cdebbfb71163fece3
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-15 20:49:49 +0530

    Create oddrange.cpp

---
+++ oddrange.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,i;
+    cin>>a>>b;
+    for(i=a+1;i<b;i++)
+    {
+        if(i%2!=0)
+        {
+            cout<<i<<" ";
+        }
+    }
+    return 0;
+}
