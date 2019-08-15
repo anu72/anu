8114438693db445333df258382bf09f4894c055f
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-15 11:51:00 +0530

    Create alphabet.cpp

---
+++ alphabet.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+  char ch;
+  cin>>ch;
+  if(isalpha(ch))
+  {
+  cout<<"Alphabet";
+  }
+  else
+  {
+  cout<<"No";
+  }
+return 0;
+}
+  
