e04588388ddf40f0eb721306e178a94d9a78e36c
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-16 13:03:38 +0530

    Create sortarr.cpp

---
+++ sortarr.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[100000],n,i;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+
+    }
+     sort(a,a+n);
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
