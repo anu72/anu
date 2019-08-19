e82f80c223abb7fc25f05fc223bbf918b5b835b8
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-19 18:44:25 +0530

    Create 44.cpp

---
+++ 44.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n==1||n==2||n==3||n==4||n==5||n==6||n==7||n==8||n==9||n==10)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+return 0;
+}
