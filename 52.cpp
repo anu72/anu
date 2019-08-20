f25cb8d882d1487c7030fad2717133034a5dc8fa
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-20 22:39:32 +0530

    Create 52.cpp

---
+++ 52.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int z;
+    cin>>z;
+    if(z==1)
+    {
+        cout<<"One";
+    }
+    else if(z==2)
+    {
+        cout<<"Two";
+    }
+    else if(z==3)
+    {
+        cout<<"Three";
+    }
+    else if(z==4)
+    {
+        cout<<"Four";
+    }
+    else if(z==5)
+    {
+        cout<<"Five";
+    }
+    else if(z==6)
+    {
+        cout<<"Six";
+    }
+    else if(z==7)
+    {
+        cout<<"Seven";
+    }
+    else if(z==8)
+    {
+        cout<<"Eight";
+    }
+    else if(z==9)
+    {
+        cout<<"Nine";
+    }
+    else if(z==10)
+    {
+        cout<<"Ten";
+    }
+    return 0;
+}
