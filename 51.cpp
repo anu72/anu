90798690153e91a8ca3fae5c94acc1325258cf68
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-20 22:31:42 +0530

    Create 51.cpp

---
+++ 51.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string d;
+    cin>>d;
+    int i;
+    for(i=0;i<d.length();i++)
+    {
+        cout<<d[i]<<" ";
+    }
+
+
+    return 0;
+}
