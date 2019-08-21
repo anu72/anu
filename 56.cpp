d7fdc63e5d3778f250d1e06738a4c3de2f7b761a
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-21 19:36:10 +0530

    Create 56.cpp

---
+++ 56.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cctype>
+using namespace std;
+int main()
+{
+    int i,c=0,v=0;
+   string s;
+   getline(cin,s);
+  for(i=0;i<s.length();i++)
+   {
+       if(isalpha(s[i]))
+       {
+           c++;
+       }
+       if(isdigit(s[i]))
+       {
+           v++;
+       }
+
+   }
+   if(c>=1&& v>=1)
+      {
+          cout<<"yes";
+      }
+      else
+        {
+            cout<<"no";
+        }
+        return 0;
+}
