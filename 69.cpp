164519346960e36a60755f1678379789ed3a97f9
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-25 11:18:01 +0530

    Create 69.cpp

---
+++ 69.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int e,f,y;
+     cin>>e>>f;
+     y=e-f;
+     if(y%2==0)
+     {
+         cout<<"even";
+     }
+     else
+     {
+         cout<<"odd";
+     }
+     return 0;
+}
