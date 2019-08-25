13afccf415b9734f349e5e885eda011acc138a17
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-25 07:16:45 +0530

    Create 64.cpp

---
+++ 64.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w,d,u;
+    cin>>w>>d;
+    u=w+d;
+    if(u%2!=0)
+    {
+        cout<<"odd";
+    }
+    else
+    {
+        cout<<"even";
+    }
+    return 0;
+
+}
