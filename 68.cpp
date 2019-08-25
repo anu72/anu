374f967c3b29707184c86ae7f1ed6319773607a1
anu72 <54067436+anu72@users.noreply.github.com>
2019-08-25 09:01:12 +0530

    Create 68.cpp

---
+++ 68.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+     int r;
+     cin>>r;
+     if(r%7==0)
+     {
+         cout<<"yes";
+     }
+     else
+     {
+         cout<<"no";
+     }
+     return 0;
+}
